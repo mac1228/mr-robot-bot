8c240f662e48f9fecb8c4ff8422005eb94c871d4
Michael Curd <mac1228@gmail.com>
2019-07-25 03:49:05 -0700

    add back message options

--- bot.js
+++ bot.js
@@ -1,53 +1,6 @@
-// // Copyright (c) Microsoft Corporation. All rights reserved.
-// // Licensed under the MIT License.
-
-// const { ActivityHandler } = require('botbuilder');
-// // const teams = require('botbuilder-teams');
-
-
-// class MrRobot extends ActivityHandler {
-//     constructor() {
-//         super();
-
-//         // const chat  = new teams.TeamsChatConnector();
-
-//         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-//         this.onMessage(async (context, next) => {
-//             const reference = context.getConversationReference(context.request);
-
-//             await context.sendActivity(reference);
-
-//             const text = context.activity.text.trim();
-//             if (text === 'join') {
-//                 await context.sendActivity(`Oh! So you'd like to become an operator. Thank you so much for helping me out :)`);
-//             } else if (text === 'leave') {
-//                 await context.sendActivity(`Awww...see ya next time!'`);
-//             } else if (text === 'vote') {
-//                 await context.sendActivity(`Hmm who hasn't been a good operator?'`);
-//             } else {
-//                 await context.sendActivity(`beep boop.'`);
-//             }
-            
-//             // By calling next() you ensure that the next BotHandler is run.
-//             await next();
-//         });
-
-//         this.onMembersAdded(async (context, next) => {
-//             const membersAdded = context.activity.membersAdded;
-//             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-//                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-//                     await context.sendActivity(`Hey! I'm Mr. Robot. Pleaseure to meet ya :)`);
-//                 }
-//             }
-//             // By calling next() you ensure that the next BotHandler is run.
-//             await next();
-//         });
-//     }
-// }
-
 'use strict';
 
-module.exports.setup = function(app) {
+module.exports.setup = (app) => {
     const builder = require('botbuilder');
     const teams = require('botbuilder-teams');
 
@@ -57,17 +10,24 @@ module.exports.setup = function(app) {
         appPassword: process.env.MicrosoftAppPassword,
         openIdMetadata: process.env.BotOpenIdMetadata
     });
-    
-    const inMemoryBotStorage = new builder.MemoryBotStorage();
-    
-    const bot = new builder.UniversalBot(connector);
 
-    bot.on('conversationUpdate', (session) => {
-        // Message might contain @mentions which we would like to strip off in the response
+    const inMemoryBotStorage = new builder.MemoryBotStorage();
+
+    const bot = new builder.UniversalBot(connector, function (session) {
+
         var text = teams.TeamsMessage.getTextWithoutMentions(session.message);
-        session.send('You said: %s', text);
+
+        if (text === 'join') {
+            session.send(`Oh! So you'd like to become an operator. Thank you so much for helping me out :)`);
+        } else if (text === 'leave') {
+            session.send(`Awww...see ya next time!'`);
+        } else if (text === 'vote') {
+            session.send(`Hmm who hasn't been a good operator?'`);
+        } else {
+            session.send(`beep boop.'`);
+        }
     });
-    
+
     bot.set('storage', inMemoryBotStorage);
 
     // Setup an endpoint on the router for the bot to listen.
