bac0010d94668e57b5bf7645890a44cc2f5e8e94
Michael Curd <mac1228@gmail.com>
2019-07-25 19:43:09 -0700

    updage botbuilder package

--- bot.js
+++ bot.js
@@ -1,19 +1,13 @@
 const { ActivityHandler, TurnContext } = require('botbuilder');
 const teams = require('botbuilder-teams');
-// const teams = require('botbuilder-teams');
+
 
 class MrRobot extends ActivityHandler {
     constructor() {
         super();
 
-        // const chat  = new teams.TeamsChatConnector();
-
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            // const reference = context.getConversationReference(context.request);
-
-            // await context.sendActivity(reference);
-
             const teamsCtx = teams.TeamsContext.from(context);
             const text = teamsCtx.getActivityTextWithoutMentions();
 
@@ -25,23 +19,6 @@ class MrRobot extends ActivityHandler {
                 await context.sendActivity(`Hmm who hasn't been a good operator?`);
             } else if (text === 'reset') {
                 await context.sendActivity(`You haven't set up this command yet`);
-                // Forget everything we know about the user
-                // session.userData = {};
-                // session.conversationData = {};
-                // session.privateConversationData = {};
-                // session.save().sendBatch();
-
-                // let conversationUpdateEvent = {
-                //     type: "conversationUpdate",
-                //     agent: message.agent,
-                //     source: message.source,
-                //     sourceEvent: message.sourceEvent,
-                //     user: message.user,
-                //     address: message.address,
-                //     timestamp: message.timestamp,
-                //     membersAdded: [message.address.user, message.address.bot],
-                // };
-                // bot.receive(conversationUpdateEvent);
             } else if (text === 'me') {
                 const ref = TurnContext.getConversationReference(context.activity);
                 const userName = ref.user.name;
